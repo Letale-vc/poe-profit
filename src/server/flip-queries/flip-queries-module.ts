f22705b09cac79b690aedb6d8fb543eaecaf329c
Letale-vc <letalerv@gmail.com>
2023-06-13 02:04:57 +0300

    small change

--- src/server/flip-queries/flip-queries-module.ts
+++ src/server/flip-queries/flip-queries-module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { FlipQueries } from '../meApp/flipQueries/FlipQueries';
 import { FlipQueriesController } from './flip-queries-controller';
+import { WorkingWithFile } from '../meApp/helpers/workingWithFile/WorkingWithFile';
 
 @Module({
   imports: [],
   controllers: [FlipQueriesController],
-  providers: [{ provide: 'FlipQueries', useValue: new FlipQueries() }],
+  providers: [
+    {
+      provide: 'FlipQueries',
+      useValue: new FlipQueries(new WorkingWithFile('poeSearchUrls.json')),
+    },
+  ],
 })
 export class FlipQueriesModule {}
