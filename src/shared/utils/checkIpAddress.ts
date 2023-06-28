5963a6a141b85c8e408c81c2fda1429ec9b9fffc
Letale-vc <letalerv@gmail.com>
2023-06-28 15:19:39 +0300

    small fix test

--- src/shared/utils/checkIpAddress.ts
+++ src/shared/utils/checkIpAddress.ts
@@ -4,12 +4,16 @@ export const checkIpAddress = (
   ipv6Address: string | string[] | undefined,
 ): boolean => {
   if (NODE_ENV === 'development') return true;
-  let ipv4Address: string | undefined;
-
+  let findAdminAddress: boolean = false
   if (typeof ipv6Address === 'string') {
-    ipv4Address = ipv6Address.replace('::ffff:', '');
+    const ipv4Address = ipv6Address.replace('::ffff:', '');
+    findAdminAddress = ipv4Address === myIpAddress
   } else if (Array.isArray(ipv6Address)) {
-    ipv4Address = ipv6Address[0]?.replace('::ffff:', '');
+    ipv6Address.forEach(el=>{
+    const  ipv4Address = el?.replace('::ffff:', '');
+    findAdminAddress = ipv4Address === myIpAddress
+    })
+    
   }
-  return ipv4Address === myIpAddress;
+  return findAdminAddress
 };
