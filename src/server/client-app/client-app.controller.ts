2536269d32e1f1a8190dc09e4d0895edfe6ee065
Letale-vc <letalerv@gmail.com>
2023-06-28 15:28:22 +0300

    small fix

--- src/server/client-app/client-app.controller.ts
+++ src/server/client-app/client-app.controller.ts
@@ -2,7 +2,6 @@ import { Controller, Get, Render } from '@nestjs/common';
 import { Req, Res } from '@nestjs/common/decorators';
 import type { Request, Response } from 'express';
 import { checkIpAddress } from '../../shared/utils/checkIpAddress';
-import { logger } from '../../MyApp/Logger/LoggerPino';
 
 @Controller()
 export class ClientAppController {
@@ -30,11 +29,10 @@ export class ClientAppController {
   settings(@Req() req: Request, @Res() res: Response) {
     const ipv6Address =
       req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-    logger.info(`IP Address: ${ipv6Address} trying to get settings`)
     const adminAddress = checkIpAddress(ipv6Address);
     if (!adminAddress) {
       res.redirect(301, '/');
-      return
+      return;
     }
     return {};
   }
--- src/shared/utils/checkIpAddress.ts
+++ src/shared/utils/checkIpAddress.ts
@@ -1,19 +1,20 @@
+import { logger } from '../../MyApp/Logger/LoggerPino';
 import { myIpAddress, NODE_ENV } from '../constants/env';
 
 export const checkIpAddress = (
   ipv6Address: string | string[] | undefined,
 ): boolean => {
+  logger.info(`IP Address: ${ipv6Address} trying to get close section`);
   if (NODE_ENV === 'development') return true;
-  let findAdminAddress: boolean = false
+  let findAdminAddress = false;
   if (typeof ipv6Address === 'string') {
     const ipv4Address = ipv6Address.replace('::ffff:', '');
-    findAdminAddress = ipv4Address === myIpAddress
+    findAdminAddress = ipv4Address === myIpAddress;
   } else if (Array.isArray(ipv6Address)) {
-    ipv6Address.forEach(el=>{
-    const  ipv4Address = el?.replace('::ffff:', '');
-    findAdminAddress = ipv4Address === myIpAddress
-    })
-    
+    ipv6Address.forEach((el) => {
+      const ipv4Address = el?.replace('::ffff:', '');
+      findAdminAddress = ipv4Address === myIpAddress;
+    });
   }
-  return findAdminAddress
+  return findAdminAddress;
 };
